2bc2d779f79e642de7d022a28da43f5e0539c6fb
Mannonen Lukas <lukas.mannonen@hotmail.fr>
2022-05-11 13:45:48 +0200

    exercice2

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   return (
+
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <Image source={require('./rover.jpg')}/>
+      <Text>Le rover Opportunity</Text>
       <StatusBar style="auto" />
     </View>
   );
